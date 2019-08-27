842f8e2ed13c1a3044f1d3bcc9e7d770bd616a8f
Nindya Hapsari <nindyahapsariiljas@yahoo.com>
2019-08-27 21:05:10 +0200

    first commit, service topic

---
+++ src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HeroesComponent } from "./heroes/heroes.component";
+import { FormsModule } from "@angular/forms";
+import { HeroDetailComponent } from "./hero-detail/hero-detail.component";
+import { MessageComponent } from './message/message.component';
+
+@NgModule({
+  declarations: [AppComponent, HeroesComponent, HeroDetailComponent, MessageComponent],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
---
+++ src/app/message/message.component.ts
@@ -0,0 +1,13 @@
+import { MessageService } from "../message.service";
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-message",
+  templateUrl: "./message.component.html",
+  styleUrls: ["./message.component.scss"]
+})
+export class MessageComponent implements OnInit {
+  constructor(public messageService: MessageService) {}
+
+  ngOnInit() {}
+}
